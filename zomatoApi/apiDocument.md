c5932e55fb280658e9e81b2050d88704769a3b82
developerfunnel@gmail.com <ahanda205@gmail.com>
2022-03-22 21:46:53 +0530

    zomatoapi

--- zomatoApi/apiDocument.md
+++ zomatoApi/apiDocument.md
@@ -1,22 +1,35 @@
 //page1 (search)
 
 > List of city
+(http://localhost:9800/location)
+(http://localhost:9800/list/location)
+
 > List of restaurants
+(http://localhost:9800/list/restaurants)
+(http://localhost:9800/restaurants)
+
 > List of MealType
+(http://localhost:9800/list/mealType)
+
 > Restaurants on the basis of city
+(http://localhost:9800/restaurants?state_id=1)
 
 //page 2
 
 > Restaurants on basis of MealType
-
+(http://localhost:9800/restaurants?meal_id=2)
 > Filter on basis of Cuisine
+(http://localhost:9800/filter/1?cuisineId=4)
 > Filter on basis of Price
+(http://localhost:9800/filter/1?lcost=500&hcost=900)
 > Sort low to high/high to low
+(http://localhost:9800/filter/1?sort=-1)
 
 //page 3
 > Restaurants Details
 > Menu on the basis of restaurants
 > List all Menu
+(http://localhost:9800/list/menu)
 
 // page 4
 > menu details on basis of item
@@ -24,6 +37,7 @@
 
 // page5
 > view all orders
+(http://localhost:9800/list/orders)
 > view orders on the basis of emailId
 > update order status
 
--- zomatoApi/app.js
+++ zomatoApi/app.js
@@ -14,6 +14,89 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors())
 
+//get Heart beat
+app.get('/',(req,res) => {
+    res.send('Welcome To Heart Beat')
+})
+
+//location
+app.get('/location',(req,res) => {
+    db.collection('location').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// List Api
+app.get('/list/:items',(req,res) => {
+    let colName = req.params.items
+    db.collection(colName).find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// restaurants on basis of meal and state 
+app.get('/restaurants',(req,res) => {
+    let stateId = Number(req.query.state_id)
+    let mealId = Number(req.query.meal_id)
+    let query = {};
+    if(stateId && mealId){
+        query = {'mealTypes.mealtype_id':mealId,state_id:stateId}
+    }else if(stateId){
+        query = {state_id:stateId}
+    }else if(mealId){
+        query = {'mealTypes.mealtype_id':mealId}
+    }
+    db.collection('restaurants').find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/// filters
+app.get(`/filter/:mealId`,(req,res) => {
+    let sort = {cost:1}
+    let skip = 0;
+    let limit = 1000000000;
+    let mealId = Number(req.params.mealId);
+    let cuisineId = Number(req.query.cuisineId);
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    let query = {}
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit);
+    }
+
+    if(cuisineId && lcost && hcost){
+        query={
+            "cuisines.cuisine_id":cuisineId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}],
+            "mealTypes.mealtype_id":mealId
+        }
+    }
+
+    if(cuisineId){
+        query={
+            "cuisines.cuisine_id":cuisineId,
+            "mealTypes.mealtype_id":mealId
+        }
+    }else if(lcost&hcost){
+        query={
+            $and:[{cost:{$gt:lcost,$lt:hcost}}],
+            "mealTypes.mealtype_id":mealId
+        }
+    }
+
+    db.collection('restaurants').find(query).sort(sort).skip(skip).limit(limit).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
 
 
 
